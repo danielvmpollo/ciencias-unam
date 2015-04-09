032a008d9d77d43df99c9041c994cc64a0394a36
guillermorojas <guillermorojas@localhost.localdomain>
2015-04-09 14:05:58 -0500

    File Upload Spring Security

---
+++ src/main/java/mx/unam/ciencias/controller/rest/LocalControllerRest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.unam.ciencias.controller.rest;
+
+import java.util.List;
+import mx.unam.ciencias.model.Local;
+import mx.unam.ciencias.service.LocalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author guillermorojas
+ */
+@Controller 
+@RequestMapping(value="/locales")
+public class LocalControllerRest {
+    
+    @Autowired
+    private LocalService localService;
+    
+    @RequestMapping(value = "/all", method = RequestMethod.GET) 
+    @ResponseBody
+    public List<Local> findAll(){
+        return  localService.findAll();
+    }
+    
+}
